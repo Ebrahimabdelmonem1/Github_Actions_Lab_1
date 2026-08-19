bb658cfa35ab7a608d8aca7b78ff9375e5b64f6f
Ebrahimabdelmonem1 <esanad912@gmail.com>
2026-08-19 16:05:20 +0300

    Debug src

--- src/test.sh
+++ src/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #src/test.sh
-EXPECTED="Hello,test"
+EXPECTED="Hello,Test"
 OUTPUT=$(node -e "console.log(require('./src/app')('Test'))") 
 if [ "$OUTPUT" == "$EXPECTED" ] ; then
 echo "Test passed"
@@ -11,5 +11,4 @@ echo "Test failed , Expected '$EXPECTED' but go '$OUTPUT'"
 exit 1
  
 fi 
- 
  
